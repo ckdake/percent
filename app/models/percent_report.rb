fc007e4c70d610598f94d1a199a026d5bef874a5
Chris Kelly <ckdake@ckdake.com>
2014-06-25 13:42:51 -0400

    A more sensible graphs

--- app/models/percent_report.rb
+++ app/models/percent_report.rb
@@ -19,37 +19,51 @@ class PercentReport
   end
 
   def my_hire_date
-    @nerd_repository.me["hire_date"]
+    Date.parse(@nerd_repository.me["hire_date"])
   end
 
   def percent_newer_than_me
-    ((total_nerds_with_hire_dates.reject { |nerd|
-      nerd["hire_date"] < Date.parse(my_hire_date)
-    }.count.to_f / total_nerds_with_hire_dates.count.to_f) * 100).round(2)
+    ((current_nerds_with_hire_date.reject { |nerd|
+      nerd["hire_date"] < my_hire_date
+    }.count.to_f / current_nerds_with_hire_date.count.to_f) * 100).round(2)
   end
 
-  def active_buckets_and_nerd_counts_js
-    Rails.cache.fetch('active_buckets_and_nerd_counts_js') do
+  def current_nerds_buckets_js
+    Rails.cache.fetch('current_nerds_buckets_js') do
       buckets = []
-      total_nerds_with_hire_dates.each do |nerd|
+      current_nerds_with_hire_date.each { |nerd|
         (nerd["hire_date"]..Date.today).each do |some_day|
           buckets[days_since_big_nerd_epoch(some_day)] ||= 0
           buckets[days_since_big_nerd_epoch(some_day)] = buckets[days_since_big_nerd_epoch(some_day)] + 1
         end
-      end
+      }
       format_for_epoch_js(buckets)
     end
   end
 
-  def inactive_buckets_and_nerd_counts_js
-    Rails.cache.fetch('inactive_buckets_and_nerd_counts_js') do
+  def all_nerds_buckets_js
+    Rails.cache.fetch('all_nerds_buckets_js') do
       buckets = []
-      total_nerds_with_hire_dates_only_inactive.each do |nerd|
+      nerds_with_hire_date.each { |nerd|
+        nerd["end_date"] = Date.today if nerd["end_date"].blank?
+        (nerd["hire_date"]..nerd["end_date"]).each do |some_day|
+          buckets[days_since_big_nerd_epoch(some_day)] ||= 0
+          buckets[days_since_big_nerd_epoch(some_day)] = buckets[days_since_big_nerd_epoch(some_day)] + 1
+        end
+      }
+      format_for_epoch_js(buckets)
+    end
+  end
+
+  def inactive_nerds_buckets_js
+    Rails.cache.fetch('inactive_nerds_buckets_js') do
+      buckets = []
+      inactive_nerds_with_hire_date.each { |nerd|
         (nerd["hire_date"]..Date.today).each do |some_day|
           buckets[days_since_big_nerd_epoch(some_day)] ||= 0
           buckets[days_since_big_nerd_epoch(some_day)] = buckets[days_since_big_nerd_epoch(some_day)] + 1
         end
-      end
+      }
       format_for_epoch_js(buckets)
     end
   end
@@ -57,36 +71,38 @@ class PercentReport
   def earliest_hire_date
     @earliest_hire_date ||= Rails.cache.fetch('earliest_hire_date') do
       min_hire_date = Date.parse(my_hire_date)
-      total_nerds_with_hire_dates_including_inactive.each do |nerd|
-        min_hire_date = nerd["hire_date"] if nerd["hire_date"] < min_hire_date
+      nerds.each do |nerd|
+        min_hire_date = nerd["hire_date"] if !nerd["hire_date"].blank? && nerd["hire_date"] < min_hire_date
       end
       min_hire_date
     end
   end
 
   private
-  def total_nerds_with_hire_dates
-    Rails.cache.fetch('total_nerds_with_hire_dates') do
-      @nerd_repository.active.reject {
-        |nerd| nerd["hire_date"].blank?
-      }
+  def nerds
+    Rails.cache.fetch('nerds') do
+      @nerd_repository.all
     end
   end
 
-  def total_nerds_with_hire_dates_including_inactive
-    Rails.cache.fetch('total_nerds_with_hire_dates_including_inactive') do
-      @nerd_repository.all.reject {
-        |nerd| nerd["hire_date"].blank?
-      }
-    end
+  def nerds_with_hire_date
+    nerds.select { |nerd| nerd["hire_date"].present? }
   end
 
-  def total_nerds_with_hire_dates_only_inactive
-    Rails.cache.fetch('total_nerds_with_hire_dates_only_inactive') do
-      @nerd_repository.all.reject {
-        |nerd| nerd["hire_date"].blank? || (nerd["active"] == true)
-      }
-    end
+  def current_nerds
+    nerds.select { |nerd| nerd["active"] == true }
+  end
+
+  def inactive_nerds
+    nerds.select { |nerd| nerd["active"] == false }
+  end
+
+  def inactive_nerds_with_hire_date
+    inactive_nerds.select { |nerd| nerd["hire_date"].present? }
+  end
+
+  def current_nerds_with_hire_date
+    current_nerds.select { |nerd| nerd["hire_date"].present? }
   end
 
   def days_since_big_nerd_epoch(some_day)
